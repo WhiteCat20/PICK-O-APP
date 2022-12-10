a16340864f2052d822d0cbb02ce0e3a3ef89a02a
Faiz Rahmadani <fzrahmadan@gmail.com>
2022-12-10 18:27:45 +0700

    success in mapping objects from personal data

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "/src/css/style-2.css";
+
+export const Jumbotron = () => {
+  return (
+    <div className="hero">
+      <div className="hero__inner">
+        <div className="hero__inner">
+          <h1 className="hero__title">
+            Yuk!! Cari Influencer untuk Endorsement
+          </h1>
+          <p className="hero__tagline">
+            Anda bisa menemukan influencer untuk promosi barang anda dengan
+            mudah disini.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/data.js
@@ -0,0 +1,46 @@
+const influencers = [
+  {
+    id: 1,
+    name: 'Faiz',
+    followers: '1K',
+    rating: '4.5',
+  },
+  {
+    id: 2,
+    name: 'Hana',
+    followers: '2K',
+    rating: '4.7',
+  },
+  {
+    id: 3,
+    name: 'Vikry',
+    followers: '2K',
+    rating: '4.4',
+  },
+  {
+    id: 4,
+    name: 'Rizqi',
+    followers: '1.5K',
+    rating: '4.6',
+  },
+  {
+    id: 5,
+    name: 'Nurika',
+    followers: '2.3K',
+    rating: '4.5',
+  },
+  {
+    id: 6,
+    name: 'Unyul',
+    followers: '3K',
+    rating: '4.9',
+  },
+  {
+    id: 7,
+    name: 'Bodak',
+    followers: '2.8K',
+    rating: '4.8',
+  },
+]
+
+export default influencers
